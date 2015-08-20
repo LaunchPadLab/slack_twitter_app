a0201aa47877385171972c77deff3a10745d8299
Paul Gonzalez <pjg5005@gmail.com>
2015-08-20 12:10:37 -0500

    Find user by team_domain

--- app/controllers/slack_responses_controller.rb
+++ app/controllers/slack_responses_controller.rb
@@ -1,16 +1,22 @@
 class SlackResponsesController < ApplicationController
 
   def create
-    puts params
+    find_user(params)
+    create_twitter_client(@user)
+    @twitter_client.update("So excited to have started at @launchpadlab this week. Learning so much already!")
   end
 
-  def create_twitter_client
+  def create_twitter_client(user)
     @twitter_client = Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['TWITTER_KEY']
       config.consumer_secret = ENV['TWITTER_SECRET_KEY']
-      config.access_token = current_user.access_token
-      config.access_token_secret = current_user.secret_access_token
+      config.access_token = user.access_token
+      config.access_token_secret = user.secret_access_token
     end
+  end
+
+  def find_user(params)
+    @user = User.find_by_team_domain(params[:team_domain])
   end
 
 end
