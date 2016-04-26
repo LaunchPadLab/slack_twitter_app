abfec12fa665d2d883efd86bec64ca773820e416
Paul Gonzalez <pjg5005@gmail.com>
2016-04-26 18:17:26 -0500

    clean up code and delete files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,7 @@
 class UsersController < ApplicationController
-  def show
-  end
-
-  def edit
-  end
-
-  def add_team_domain
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    redirect_to root_path
   end
 end
