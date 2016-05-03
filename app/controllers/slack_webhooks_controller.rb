e3d33566d93879baf412769d56cac6fe620f3dbb
Rachel Killackey <rachel-killackey@Rachels-MacBook-Pro-2.local>
2016-05-03 15:18:37 -0500

    raising error if cached values are nil

--- app/controllers/slack_webhooks_controller.rb
+++ app/controllers/slack_webhooks_controller.rb
@@ -5,24 +5,31 @@ class SlackWebhooksController < ApplicationController
       render json: { text: "Sorry we couldn't find you. Double check that the team domain name on Slack matches." }
     else
       create_message
-      create_twitter_client(@user)
 
       begin
+        create_twitter_client(@user)
         send_tweet(@twitter_client, @message)
         render json: { text: "Cha ching!" }
       rescue Twitter::Error => e
         render json: { text: "Sorry that didn't work: #{e.message}" }
+      rescue ArgumentError => e
+        render json: { text: "Twitter login credentials have expired. Please re-authenticate." }
       end
     end
   end
 
   private
     def create_twitter_client(user)
+      access_token = Rails.cache.read('access_token')
+      secret_access_token = Rails.cache.read('secret_access_token')
+      if access_token.nil? || secret_access_token.nil?
+        raise ArgumentError
+      end
       @twitter_client = Twitter::REST::Client.new do |config|
         config.consumer_key = ENV['TWITTER_KEY']
         config.consumer_secret = ENV['TWITTER_SECRET_KEY']
-        config.access_token = Rails.cache.read('access_token')
-        config.access_token_secret = Rails.cache.read('secret_access_token')
+        config.access_token = access_token
+        config.access_token_secret = secret_access_token
       end
     end
 
